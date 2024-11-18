5dbf54eb89decfa92b5d540397c3b5b3456385e0
JDLimaco <juan_camarena2003@hotmail.com>
2024-11-18 12:38:36 -0500

    Reimplementacion php

---
+++ demo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <header>
+        --------------------Este es el header-------------------------
+    </header>
+    <main>
+        Este es el main
+        <div>
+            <?php echo 'Esto fue escrito con php' ?> 
+        </div>
+        <div>
+            Aqui ira la imagen
+            <div>
+                <img src="./FotoChamber.jpg" alt="">
+            </div>
+        </div>
+    </main>
+    <footer>
+        Este es el footer
+    </footer>
+</body>
+</html>
